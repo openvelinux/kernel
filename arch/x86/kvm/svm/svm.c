8e67498c3d9fb920345e003b1db4295788cf56bc
Like Xu <likexu@tencent.com>
2021-11-17 16:03:04 +0800

    KVM: x86/svm: Add module param to control PMU virtualization
    commit b1d66dad65dcc8a6e5942db27027a086aa4f5c16 upstream.
    
    For Intel, the guest PMU can be disabled via clearing the PMU CPUID.
    For AMD, all hw implementations support the base set of four
    performance counters, with current mainstream hardware indicating
    the presence of two additional counters via X86_FEATURE_PERFCTR_CORE.
    
    In the virtualized world, the AMD guest driver may detect
    the presence of at least one counter MSR. Most hypervisor
    vendors would introduce a module param (like lbrv for svm)
    to disable PMU for all guests.
    
    Another control proposal per-VM is to pass PMU disable information
    via MSR_IA32_PERF_CAPABILITIES or one bit in CPUID Fn4000_00[FF:00].
    Both of methods require some guest-side changes, so a module
    parameter may not be sufficiently granular, but practical enough.
    
    Signed-off-by: Like Xu <likexu@tencent.com>
    Message-Id: <20211117080304.38989-1-likexu@tencent.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -187,6 +187,10 @@ module_param(vls, int, 0444);
 static int vgif = true;
 module_param(vgif, int, 0444);
 
+/* enable/disable PMU virtualization */
+bool pmu = true;
+module_param(pmu, bool, 0444);
+
 /*
  * enable / disable AVIC.  Because the defaults differ for APICv
  * support between VMX and SVM we cannot use module_param_named.
@@ -957,6 +961,10 @@ static __init void svm_set_cpu_caps(void)
 	    boot_cpu_has(X86_FEATURE_AMD_SSBD))
 		kvm_cpu_cap_set(X86_FEATURE_VIRT_SSBD);
 
+	/* AMD PMU PERFCTR_CORE CPUID */
+	if (pmu && boot_cpu_has(X86_FEATURE_PERFCTR_CORE))
+		kvm_cpu_cap_set(X86_FEATURE_PERFCTR_CORE);
+
 	/* CPUID 0x8000001F (SME/SEV features) */
 	sev_set_cpu_caps();
 }
@@ -1077,6 +1085,9 @@ static __init int svm_hardware_setup(void)
 		else
 			pr_info("Virtual GIF supported\n");
 	}
+
+	if (!pmu)
+		pr_info("PMU virtualization is disabled\n");
 
 	svm_set_cpu_caps();
 
