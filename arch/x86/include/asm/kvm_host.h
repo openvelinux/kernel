238bbf4958eea49b22003d6a56f40309f19258ff
Like Xu <likexu@tencent.com>
2022-09-23 00:13:55 +0000

    KVM: x86/pmu: Defer counter emulated overflow via pmc->prev_counter
    commit de0f619564f4713bd548b82d535a954ffa1ee7d8 upstream.
    
    Defer reprogramming counters and handling overflow via KVM_REQ_PMU
    when incrementing counters.  KVM skips emulated WRMSR in the VM-Exit
    fastpath, the fastpath runs with IRQs disabled, skipping instructions
    can increment and reprogram counters, reprogramming counters can
    sleep, and sleeping is disallowed while IRQs are disabled.
    
     [*] BUG: sleeping function called from invalid context at kernel/locking/mutex.c:580
     [*] in_atomic(): 1, irqs_disabled(): 1, non_block: 0, pid: 2981888, name: CPU 15/KVM
     [*] preempt_count: 1, expected: 0
     [*] RCU nest depth: 0, expected: 0
     [*] INFO: lockdep is turned off.
     [*] irq event stamp: 0
     [*] hardirqs last  enabled at (0): [<0000000000000000>] 0x0
     [*] hardirqs last disabled at (0): [<ffffffff8121222a>] copy_process+0x146a/0x62d0
     [*] softirqs last  enabled at (0): [<ffffffff81212269>] copy_process+0x14a9/0x62d0
     [*] softirqs last disabled at (0): [<0000000000000000>] 0x0
     [*] Preemption disabled at:
     [*] [<ffffffffc2063fc1>] vcpu_enter_guest+0x1001/0x3dc0 [kvm]
     [*] CPU: 17 PID: 2981888 Comm: CPU 15/KVM Kdump: 5.19.0-rc1-g239111db364c-dirty #2
     [*] Call Trace:
     [*]  <TASK>
     [*]  dump_stack_lvl+0x6c/0x9b
     [*]  __might_resched.cold+0x22e/0x297
     [*]  __mutex_lock+0xc0/0x23b0
     [*]  perf_event_ctx_lock_nested+0x18f/0x340
     [*]  perf_event_pause+0x1a/0x110
     [*]  reprogram_counter+0x2af/0x1490 [kvm]
     [*]  kvm_pmu_trigger_event+0x429/0x950 [kvm]
     [*]  kvm_skip_emulated_instruction+0x48/0x90 [kvm]
     [*]  handle_fastpath_set_msr_irqoff+0x349/0x3b0 [kvm]
     [*]  vmx_vcpu_run+0x268e/0x3b80 [kvm_intel]
     [*]  vcpu_enter_guest+0x1d22/0x3dc0 [kvm]
    
    Add a field to kvm_pmc to track the previous counter value in order
    to defer overflow detection to kvm_pmu_handle_event() (the counter must
    be paused before handling overflow, and that may increment the counter).
    
    Opportunistically shrink sizeof(struct kvm_pmc) a bit.
    
    Suggested-by: Wanpeng Li <wanpengli@tencent.com>
    Fixes: 9cd803d496e7 ("KVM: x86: Update vPMCs when retiring instructions")
    Signed-off-by: Like Xu <likexu@tencent.com>
    Link: https://lore.kernel.org/r/20220831085328.45489-6-likexu@tencent.com
    [sean: avoid re-triggering KVM_REQ_PMU on overflow, tweak changelog]
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20220923001355.3741194-5-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -484,7 +484,10 @@ enum pmc_type {
 struct kvm_pmc {
 	enum pmc_type type;
 	u8 idx;
+	bool is_paused;
+	bool intr;
 	u64 counter;
+	u64 prev_counter;
 	u64 eventsel;
 	struct perf_event *perf_event;
 	struct kvm_vcpu *vcpu;
@@ -494,8 +497,6 @@ struct kvm_pmc {
 	 * ctrl value for fixed counters.
 	 */
 	u64 current_config;
-	bool is_paused;
-	bool intr;
 };
 
 /* More counters may conflict with other existing Architectural MSRs */
