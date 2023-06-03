f6927af9fd488c4de868047f7d03325f4cc88ee1
Like Xu <likexu@tencent.com>
2023-06-02 18:10:51 -0700

    KVM: x86/pmu: Provide Intel PMU's pmc_is_enabled() as generic x86 code
    commit 13afa29ae489d9b7c1038179f1e2bec74873e471 upstream.
    
    Move the Intel PMU implementation of pmc_is_enabled() to common x86 code
    as pmc_is_globally_enabled(), and drop AMD's implementation.  AMD PMU
    currently supports only v1, and thus not PERF_GLOBAL_CONTROL, thus the
    semantics for AMD are unchanged.  And when support for AMD PMU v2 comes
    along, the common behavior will also Just Work.
    
    Signed-off-by: Like Xu <likexu@tencent.com>
    Co-developed-by: Sean Christopherson <seanjc@google.com>
    Link: https://lore.kernel.org/r/20230603011058.1038821-6-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: chaithanyaLagisetty <LagisettyVenkata.NagaChaitanya@amd.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>

--- arch/x86/kvm/vmx/pmu_intel.c
+++ arch/x86/kvm/vmx/pmu_intel.c
@@ -87,17 +87,6 @@ static unsigned int intel_pmc_perf_hw_id(struct kvm_pmc *pmc)
 	return intel_arch_events[i].event_type;
 }
 
-/* check if a PMC is enabled by comparing it with globl_ctrl bits. */
-static bool intel_pmc_is_enabled(struct kvm_pmc *pmc)
-{
-	struct kvm_pmu *pmu = pmc_to_pmu(pmc);
-
-	if (!kvm_pmu_has_perf_global_ctrl(pmu))
-		return true;
-
-	return test_bit(pmc->idx, (unsigned long *)&pmu->global_ctrl);
-}
-
 /* returns 0 if idx's corresponding MSR exists; otherwise returns 1. */
 static int intel_is_valid_rdpmc_ecx(struct kvm_vcpu *vcpu, unsigned int idx)
 {
@@ -763,7 +752,7 @@ void intel_pmu_cross_mapped_check(struct kvm_pmu *pmu)
 		pmc = intel_pmc_idx_to_pmc(pmu, bit);
 
 		if (!pmc || !pmc_speculative_in_use(pmc) ||
-		    !intel_pmc_is_enabled(pmc) || !pmc->perf_event)
+		    !pmc_is_globally_enabled(pmc) || !pmc->perf_event)
 			continue;
 
 		/*
@@ -778,7 +767,6 @@ void intel_pmu_cross_mapped_check(struct kvm_pmu *pmu)
 
 struct kvm_pmu_ops intel_pmu_ops __initdata = {
 	.pmc_perf_hw_id = intel_pmc_perf_hw_id,
-	.pmc_is_enabled = intel_pmc_is_enabled,
 	.pmc_idx_to_pmc = intel_pmc_idx_to_pmc,
 	.rdpmc_ecx_to_pmc = intel_rdpmc_ecx_to_pmc,
 	.msr_idx_to_pmc = intel_msr_idx_to_pmc,
