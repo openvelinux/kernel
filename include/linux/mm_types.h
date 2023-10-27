73149b217a0078854b66894c2504944e101d74a1
Tina Zhang <tina.zhang@intel.com>
2023-10-27 08:05:23 +0800

    mm: Add structure to keep sva information
    commit 541a3e257d48c16b77d19f39ed939ef5832046df upstream.
    
    Introduce iommu_mm_data structure to keep sva information (pasid and the
    related sva domains). Add iommu_mm pointer, pointing to an instance of
    iommu_mm_data structure, to mm.
    
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Tested-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Tina Zhang <tina.zhang@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20231027000525.1278806-5-tina.zhang@intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Donghai Qiao <donghai.qiao@bytedance.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -738,6 +738,7 @@ struct mm_cid {
 #endif
 
 struct kioctx_table;
+struct iommu_mm_data;
 struct mm_struct {
 	struct {
 		/*
@@ -951,6 +952,7 @@ struct mm_struct {
 
 #ifdef CONFIG_IOMMU_MM_DATA
 		u32 pasid;
+		struct iommu_mm_data *iommu_mm;
 #endif
 #ifdef CONFIG_KSM
 		/*
