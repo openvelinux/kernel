043650bd703dcf110e5d02a389784235aec5771c
H. Peter Anvin (Intel) <hpa@zytor.com>
2023-12-05 02:50:00 -0800

    x86/cpu: Add X86_CR4_FRED macro
    commit ff45746fbf005f96e42bea466698e3fdbf926013 upstream.
    
    Add X86_CR4_FRED macro for the FRED bit in %cr4. This bit must not be
    changed after initialization, so add it to the pinned CR4 bits.
    
    Intel-SIG: commit ff45746fbf00 x86/cpu: Add X86_CR4_FRED macro.
    
    Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>
    Signed-off-by: Xin Li <xin3.li@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Tested-by: Shan Kang <shan.kang@intel.com>
    Link: https://lore.kernel.org/r/20231205105030.8698-12-xin3.li@intel.com
    
    Conflicts:
        arch/x86/include/uapi/asm/processor-flags.h
        arch/x86/kernel/cpu/common.c
    [ Xingjiang Lu: resolve context conflicts ]
    Signed-off-by: Lu, Xingjiang <xingjiang.lu@intel.com>

--- arch/x86/include/uapi/asm/processor-flags.h
+++ arch/x86/include/uapi/asm/processor-flags.h
@@ -131,6 +131,13 @@
 #define X86_CR4_PKE_BIT		22 /* enable Protection Keys support */
 #define X86_CR4_PKE		_BITUL(X86_CR4_PKE_BIT)
 
+#ifdef __x86_64__
+#define X86_CR4_FRED_BIT	32 /* enable FRED kernel entry */
+#define X86_CR4_FRED		_BITUL(X86_CR4_FRED_BIT)
+#else
+#define X86_CR4_FRED		(0)
+#endif
+
 /*
  * x86-64 Task Priority Register, CR8
  */
