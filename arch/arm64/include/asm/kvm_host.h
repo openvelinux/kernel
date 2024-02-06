f4122165da67148b4cf9dabe1e27c023db1118b5
Yanan Wang <wangyanan55@huawei.com>
2024-02-06 21:34:55 +0800

    downstream: KVM: arm64: Only probe Hisi ncsnp feature on Hisi CPUs
    The "ncsnp" is an implementation specific CPU virtualization
    feature on Hisi 1620 series CPUs. This feature works just
    like ARM standard S2FWB to reduce some cache management
    operations in virtualization.
    Given that it's Hisi specific feature, let's restrict the
    detection only to Hisi CPUs. To realize this:
    1) Add a sub-directory `hisilicon/` within arch/arm64/kvm to
    hold code for Hisi specific virtualization features.
    2) Add a new kconfig option `CONFIG_KVM_HISI_VIRT` for users
    to select the whole Hisi specific virtualization features.
    3) Add a generic global KVM variable `kvm_ncsnp_support`
    which is `false` by default. Only re-initialize it when
    we have `CONFIG_KVM_HISI_VIRT` enabled.
    
    Signed-off-by: Yanan Wang <wangyanan55@huawei.com>
    Signed-off-by: lishusen <lishusen2@huawei.com>
    Signed-off-by: Slim6882 <15605196882@163.com>
    Signed-off-by: chenyi <chenyi211@h-partners.com>
    Signed-off-by: Jinhui Guo <guojinhui.liam@bytedance.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -27,7 +27,6 @@
 #include <asm/fpsimd.h>
 #include <asm/kvm.h>
 #include <asm/kvm_asm.h>
-#include <asm/hisi_cpu_model.h>
 
 #define __KVM_HAVE_ARCH_INTC_INITIALIZED
 
@@ -1203,5 +1202,6 @@ void kvm_arm_vcpu_power_off(struct kvm_vcpu *vcpu);
 bool kvm_arm_vcpu_stopped(struct kvm_vcpu *vcpu);
 
 extern bool force_wfi_trap;
+extern bool kvm_ncsnp_support;
 
 #endif /* __ARM64_KVM_HOST_H__ */
