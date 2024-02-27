ae7e17571a30d8bcee44085f911dda022194e982
Marc Zyngier <maz@kernel.org>
2024-02-27 08:27:54 +0000

    KVM: arm64: Exclude mdcr_el2_host from kvm_vcpu_arch
    commit 4bacd723705a6b6c8386daf3d5148aca66135f3c upstream.
    
    As for the rest of the host debug state, the host copy of mdcr_el2
    has little to do in the vcpu, and is better placed in the host_data
    structure.
    
    Reviewed-by : Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Junhao He <hejunhao3@huawei.com>
    Signed-off-by: Slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Slim6882 <15605196882@163.com>
    Signed-off-by: chenyi <chenyi211@h-partners.com>
    Signed-off-by: Jinhui Guo <guojinhui.liam@bytedance.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -456,6 +456,8 @@ struct kvm_host_data {
 		u64 pmscr_el1;
 		/* Self-hosted trace */
 		u64 trfcr_el1;
+		/* Values of trap registers for the host before guest entry. */
+		u64 mdcr_el2;
 	} host_debug_state;
 };
 
@@ -514,9 +516,6 @@ struct kvm_vcpu_arch {
 	u64 hcr_el2;
 	u64 mdcr_el2;
 	u64 cptr_el2;
-
-	/* Values of trap registers for the host before guest entry. */
-	u64 mdcr_el2_host;
 
 	/* Exception Information */
 	struct kvm_vcpu_fault_info fault;
