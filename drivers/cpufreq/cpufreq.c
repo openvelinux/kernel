ed707f2739d70ce45a82db91ac666694803e8e04
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2025-03-28 21:48:54 +0100

    cpufreq: Pass policy pointer to ->update_limits()
    commit eaff6b62d3439ca6ee00dba4f77673a8c37dac20 upstream.
    
    Since cpufreq_update_limits() obtains a cpufreq policy pointer for the
    given CPU and reference counts the corresponding policy object, it may
    as well pass the policy pointer to the cpufreq driver's ->update_limits()
    callback which allows that callback to avoid invoking cpufreq_cpu_get()
    for the same CPU.
    
    Accordingly, redefine ->update_limits() to take a policy pointer instead
    of a CPU number and update both drivers implementing it, intel_pstate
    and amd-pstate, as needed.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Acked-by: Sudeep Holla <sudeep.holla@arm.com>
    Tested-by: Sudeep Holla <sudeep.holla@arm.com>
    Link: https://patch.msgid.link/8560367.NyiUUSuA9g@rjwysocki.net
    Signed-off-by: Mukesh Ogare <OgreKumar.Mukesh@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2759,7 +2759,7 @@ void cpufreq_update_limits(unsigned int cpu)
 		return;
 
 	if (cpufreq_driver->update_limits)
-		cpufreq_driver->update_limits(cpu);
+		cpufreq_driver->update_limits(policy);
 	else
 		cpufreq_update_policy(cpu);
 
