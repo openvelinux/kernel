41765b049695378b47c1c93b089f2f6f99261b6d
Baihan Li <libaihan@huawei.com>
2025-08-13 17:42:28 +0800

    drm/hisilicon/hibmc: fix the i2c device resource leak when vdac init failed
    commit e5f48bfa2ae0806d5f51fb8061afc619a73599a7 upstream.
    
    Currently the driver missed to clean the i2c adapter when vdac init failed.
    It may cause resource leak.
    
    Fixes: a0d078d06e516 ("drm/hisilicon: Features to support reading resolutions from EDID")
    Signed-off-by: Baihan Li <libaihan@huawei.com>
    Signed-off-by: Yongbang Shi <shiyongbang@huawei.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20250813094238.3722345-2-shiyongbang@huawei.com
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: slim6882 <yangjunshuo@huawei.com>

--- drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_vdac.c
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_vdac.c
@@ -47,7 +47,7 @@ static void hibmc_connector_destroy(struct drm_connector *connector)
 {
 	struct hibmc_vdac *vdac = to_hibmc_vdac(connector);
 
-	i2c_del_adapter(&vdac->adapter);
+	hibmc_ddc_del(vdac);
 	drm_connector_cleanup(connector);
 }
 
@@ -104,7 +104,7 @@ int hibmc_vdac_init(struct hibmc_drm_private *priv)
 	ret = drm_simple_encoder_init(dev, encoder, DRM_MODE_ENCODER_DAC);
 	if (ret) {
 		drm_err(dev, "failed to init encoder: %d\n", ret);
-		return ret;
+		goto err;
 	}
 
 	drm_encoder_helper_add(encoder, &hibmc_encoder_helper_funcs);
@@ -115,7 +115,7 @@ int hibmc_vdac_init(struct hibmc_drm_private *priv)
 					  &vdac->adapter);
 	if (ret) {
 		drm_err(dev, "failed to init connector: %d\n", ret);
-		return ret;
+		goto err;
 	}
 
 	drm_connector_helper_add(connector, &hibmc_connector_helper_funcs);
@@ -125,4 +125,9 @@ int hibmc_vdac_init(struct hibmc_drm_private *priv)
 	connector->polled = DRM_CONNECTOR_POLL_CONNECT | DRM_CONNECTOR_POLL_DISCONNECT;
 
 	return 0;
+
+err:
+	hibmc_ddc_del(vdac);
+
+	return ret;
 }
