intel-lab-lkp/linux

KVM: x86/mmu: Rename .private_max_mapping_level() to .gmem_max_mapping_level()
Rename kvm_x86_ops.private_max_mapping_level() to .gmem_max_mapping_level()
in anticipation of extending guest_memfd support to non-private memory.

No functional change intended.

Reviewed-by: Xiaoyao Li <xiaoyao.li@intel.com>
Acked-by: David Hildenbrand <david@redhat.com>
Signed-off-by: Ackerley Tng <ackerleytng@google.com>
Signed-off-by: Fuad Tabba <tabba@google.com>
Co-developed-by: Sean Christopherson <seanjc@google.com>
Signed-off-by: Sean Christopherson <seanjc@google.com>

arch/x86/kvm/mmu/mmu.c
@@ -4532,7 +4532,7 @@ static u8 kvm_max_private_mapping_level(struct kvm *kvm, kvm_pfn_t pfn,
 	if (max_level == PG_LEVEL_4K)
 		return PG_LEVEL_4K;
 
-	req_max_level = kvm_x86_call(private_max_mapping_level)(kvm, pfn);
+	req_max_level = kvm_x86_call(gmem_max_mapping_level)(kvm, pfn);
 	if (req_max_level)
 		max_level = min(max_level, req_max_level);
 

openvelinux/kernel

commit d6c840adfebca55d4520244c77467bd16b4ea41e
Reference:https://github.com/AMDESE/linux/commit/d6c840adfebca55d4520244c77467bd16b4ea41e
[Backport Changes]
1. In arch/x86/kvm/mmu/mmu.c, in function kvm_max_private_mapping_level(),
instead of kvm_x86_call() macro usage of existing static call macro
static_call() was retained since the upstream commit 896046474f8d2e
(KVM: x86: Introduce kvm_x86_call() to simplify static calls of
kvm_x86_ops) which introduces macro  kvm_x86_call() is not available
in the current source tree.
2. Changes related to files arch/x86/kvm/vmx/main.c, arch/x86/kvm/vmx/tdx.c
and arch/x86/kvm/vmx/x86_ops.h were skipped as they are out of scope for
this backport.
Reviewed-by: Fuad Tabba <tabba@google.com>
Message-ID: <20250729225455.670324-13-seanjc@google.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
Signed-off-by: Arukonda Rahul <arukonda.rahul@amd.com>
Signed-off-by: Rui Qi <qirui.001@bytedance.com>

arch/x86/kvm/mmu/mmu.c
@@ -4345,7 +4345,7 @@ static u8 kvm_max_private_mapping_level(struct kvm *kvm, kvm_pfn_t pfn,
 	if (max_level == PG_LEVEL_4K)
 		return PG_LEVEL_4K;
 
-	req_max_level = static_call(kvm_x86_private_max_mapping_level)(kvm, pfn);
+	req_max_level = static_call(kvm_x86_gmem_max_mapping_level)(kvm, pfn);
 	if (req_max_level)
 		max_level = min(max_level, req_max_level);
 