ekuiter/torte-linux

platform/x86: ISST: Add suspend/resume callbacks
To support S3/S4 with TPMI interface add suspend/resume callbacks.
Here HW state is stored in suspend callback and restored during
resume callback.

The hardware state which needs to be stored/restored:
- CLOS configuration
- CLOS Association
- SST-CP enable/disable status
- SST-PP perf level setting

Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
Suggested-by: Hans de Goede <hdegoede@redhat.com>
Reviewed-by: Zhang Rui <rui.zhang@intel.com>
Tested-by: Pragya Tanwar <pragya.tanwar@intel.com>
Link: https://lore.kernel.org/r/20230308070642.1727167-9-srinivas.pandruvada@linux.intel.com
Reviewed-by: Hans de Goede <hdegoede@redhat.com>
Signed-off-by: Hans de Goede <hdegoede@redhat.com>

drivers/platform/x86/intel/speed_select_if/isst_tpmi.c
@@ -34,6 +34,22 @@ static void intel_sst_remove(struct auxiliary_device *auxdev)
 	tpmi_sst_exit();
 }
 
+static int intel_sst_suspend(struct device *dev)
+{
+	tpmi_sst_dev_suspend(to_auxiliary_dev(dev));
+
+	return 0;
+}
+
+static int intel_sst_resume(struct device *dev)
+{
+	tpmi_sst_dev_resume(to_auxiliary_dev(dev));
+
+	return 0;
+}
+
+static DEFINE_SIMPLE_DEV_PM_OPS(intel_sst_pm, intel_sst_suspend, intel_sst_resume);
+
 static const struct auxiliary_device_id intel_sst_id_table[] = {
 	{ .name = "intel_vsec.tpmi-sst" },
 	{}
@@ -44,6 +60,9 @@ static struct auxiliary_driver intel_sst_aux_driver = {
 	.id_table       = intel_sst_id_table,
 	.remove         = intel_sst_remove,
 	.probe          = intel_sst_probe,
+	.driver = {
+		.pm = pm_sleep_ptr(&intel_sst_pm),
+	},
 };
 
 module_auxiliary_driver(intel_sst_aux_driver);

openvelinux/kernel

commit 91576acab0203062b93a578a7d05c8fe9e527f80 upstream.
Signed-off-by: Yunhui Cui <cuiyunhui@bytedance.com>

drivers/platform/x86/intel/speed_select_if/isst_tpmi.c
@@ -34,6 +34,22 @@ static void intel_sst_remove(struct auxiliary_device *auxdev)
 	tpmi_sst_exit();
 }
 
+static int intel_sst_suspend(struct device *dev)
+{
+	tpmi_sst_dev_suspend(to_auxiliary_dev(dev));
+
+	return 0;
+}
+
+static int intel_sst_resume(struct device *dev)
+{
+	tpmi_sst_dev_resume(to_auxiliary_dev(dev));
+
+	return 0;
+}
+
+static SIMPLE_DEV_PM_OPS(intel_sst_pm, intel_sst_suspend, intel_sst_resume);
+
 static const struct auxiliary_device_id intel_sst_id_table[] = {
 	{ .name = "intel_vsec.tpmi-sst" },
 	{}
@@ -44,6 +60,9 @@ static struct auxiliary_driver intel_sst_aux_driver = {
 	.id_table       = intel_sst_id_table,
 	.remove         = intel_sst_remove,
 	.probe          = intel_sst_probe,
+	.driver = {
+		.pm = pm_ptr(&intel_sst_pm),
+	},
 };
 
 module_auxiliary_driver(intel_sst_aux_driver);